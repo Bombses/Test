b78b4bdeba57227a9f501ba8cfaf1f1fd7f7b51e
Mathieu Hebert <Mathieu.hebert@ccnb.ca>
2016-10-06 11:49:55 -0300

    Test

---
+++ tset/tset/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace tset
+{
+    [Activity(Label = "tset", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
+            int x = 1;
+        }
+    }
+}
+
